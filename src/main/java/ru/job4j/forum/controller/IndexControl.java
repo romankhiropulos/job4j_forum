bb5af7822033ba6c4d2de1885f666ea5ca6a5798
romankhiropulos <romankhiropulos@gmail.com>
2021-11-18 23:43:07 +0300

    main tests in file with equals name

--- src/main/java/ru/job4j/forum/controller/IndexControl.java
+++ src/main/java/ru/job4j/forum/controller/IndexControl.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexControl {
+
     @GetMapping({"/", "/index"})
     public String index() {
         return "index";
